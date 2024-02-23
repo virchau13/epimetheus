6c03aeecc83cfea64f4d74b71ddd91b60010c631
Vir Chaudhury <virchau13@hexular.net>
2024-02-23 16:57:56 +0800

    chore(nix): add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Discord bot";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem(system: let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages = rec {
+      epimetheus = with pkgs; rustPlatform.buildRustPackage  {
+        pname = "epimetheus";
+        version = "0.1.0";
+
+        src = ./.;
+
+        buildInputs = [ m4 ];
+        nativeBuildInputs = [ m4 ];
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+      };
+      default = epimetheus;
+    };
+    devShell = with pkgs; stdenv.mkDerivation {
+      buildInputs = [ m4 ];
+    };
+  });
+}
